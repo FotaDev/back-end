05d38074869b996384cbb09b92b016cb86689962
Carlos <charliedn@Gmail.com>
2018-02-27 20:37:17 +0000

    s

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth' 
+  mount_devise_token_auth_for 'User', at: 'auth'
+
   resources :hires
   root to: 'hires#index'
 
