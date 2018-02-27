0ee1368e6ecdc7b870a38c405a15acb205ac8095
Carlos <charliedn@Gmail.com>
2018-02-27 18:47:46 +0000

    Strong params added Group_id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_filter :configure_devise_params, if: devise_controller?
+
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
+
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:email,:password, :group_id)
+    end
+  end
 end
