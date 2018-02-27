3824ab909a780badb4d7346ebe6fe0d5a514508f
Carlos <charliedn@Gmail.com>
2018-02-27 20:45:38 +0000

    I hope this works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,6 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
-
-  respond_to :json
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :group_id
-  end
-
 end
+
