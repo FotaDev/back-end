52bbfbeb6951dd83f081e59625956ad933deea8c
BenassiJosef <benassisolutions@gmail.com>
2018-03-01 04:32:56 +0000

    working
    working with front end now

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth', :controllers => { registrations: 'registrations' }
+  mount_devise_token_auth_for 'User', at: 'auth'
+  
+  # , :controllers => { registrations: 'registrations' }
 
 
-  resources :hires
-  root to: 'hires#index'
+  # resources :hires
+  # root to: 'hires#index'
 
 # devise_for :users
 
