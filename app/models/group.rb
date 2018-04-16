2a3b36a077f1a47edc44eeccbabbd86257344e08
Carlos <charliedn@gmail.com>
2018-04-16 10:31:08 +0100

    group model validation@

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # == Schema Information
 #
 # Table name: groups
@@ -11,26 +10,10 @@
 
 class Group < ApplicationRecord
   has_many :users
-  has_many :hires
+  has_many :hires 
   validates :name, presence: true, uniqueness: true
-end
-=======
-# == Schema Information
-#
-# Table name: groups
-#
-#  id      :integer          not null, primary key
-#  name    :string(255)
-#  address :text(65535)
-#
-
-
-class Group < ApplicationRecord
-  has_many :users
-  has_many :hires
 
   def as_json(options=nil)
     super(include: { hires: { include: { orders: { include: { item: { include: :size } } } } } })
   end
 end
->>>>>>> cors-fix
